09e3343d3b0180373c8d762d9bca93932019a128
Nathaniel Potrepka <nspotrepka@gmail.com>
2019-10-25 05:18:06 -0700

    Add dummy models

---
+++ models/image.py
@@ -0,0 +1,29 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+class ImageEncoder:
+    def __init__(self, width, height, channels):
+        self.model = keras.Sequential()
+
+        model.add(Convolution2D(32, 3, 3, border_mode='same',
+                                input_shape=(width, height, channels)))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(32, 3, 3, border_mode='same'))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Convolution2D(64, 3, 3, border_mode='same'))
+        model.add(Activation('relu'))
+        model.add(Convolution2D(64, 3, 3,border_mode='same'))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(0.25))
+
+        model.add(Flatten())
+        model.add(Dense(512))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(10))
+        model.add(Activation('softmax'))
---
+++ models/music.py
@@ -0,0 +1,7 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+
+class MusicEncoder:
+    def __init__(self):
+        self.model = keras.Sequential()
