8bd9813ec3f98ae83a18c2c6aad55b73622c8d0b
nspotrepka <nspotrepka@gmail.com>
2019-11-22 09:18:25 -0800

    Change default pool size to 50

--- models/cyclegan/model.py
+++ models/cyclegan/model.py
@@ -24,7 +24,7 @@ class CycleGAN(pl.LightningModule):
                  loader, in_channels, out_channels, g_filters=64, d_filters=64,
                  residual_layers=9, dropout=False, learning_rate=0.0002,
                  beta_1=0.5, beta_2=0.999, init_type='normal', init_scale=0.02,
-                 pool_size=0, lambda_a=10.0, lambda_b=10.0, lambda_id=0.0,
+                 pool_size=50, lambda_a=10.0, lambda_b=10.0, lambda_id=0.0,
                  epochs=200):
         super(CycleGAN, self).__init__()
 
