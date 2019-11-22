ba0f0ae53ca921d35ee66f96186f7fdce0d59671
nspotrepka <nspotrepka@gmail.com>
2019-11-22 08:22:23 -0800

    Move detach

--- models/cyclegan/model.py
+++ models/cyclegan/model.py
@@ -138,14 +138,14 @@ class CycleGAN(pl.LightningModule):
 
     def backward_d_func(self, net, real, fake):
         loss_real = self.loss_func_gan(net(real), True)
-        loss_fake = self.loss_func_gan(net(fake.detach()), False)
+        loss_fake = self.loss_func_gan(net(fake), False)
         return loss_real + loss_fake
 
     def backward_d(self):
         device_a = next(self.dis_a.parameters()).device
         device_b = next(self.dis_b.parameters()).device
-        fake_a = self.fake_a_pool.query(self.fake_a)
-        fake_b = self.fake_b_pool.query(self.fake_b)
+        fake_a = self.fake_a_pool.query(self.fake_a).detach()
+        fake_b = self.fake_b_pool.query(self.fake_b).detach()
         fake_a.to(device_a)
         fake_b.to(device_b)
         self.loss_d_a = self.backward_d_func(self.dis_a, self.real_a, fake_a)
